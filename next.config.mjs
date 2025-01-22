9190f97e6b5c65599c4479f7779ae16ec91ee763
ayushjrathod <clashofa1057@gmail.com>
2025-01-22 18:44:24 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api2/:path*",
+        destination: "http://127.0.0.1:8000/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api2/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
