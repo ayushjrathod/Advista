0dbad555e2b1b51714b49e673aa013d25b9aaccf
ayushjrathod <clashofa1057@gmail.com>
2025-01-22 18:32:01 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,28 @@ const nextConfig = {
     return [
       {
         source: "/api2/:path*",
-        destination: "http://20.198.16.49:8000/:path*",
+        destination: "http://127.0.0.1:8000/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api2/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
       },
     ];
   },
