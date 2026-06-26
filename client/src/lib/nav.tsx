1b92535e400b759b181585ff2f3114f68eb9efda
ayushjrathod <clashofa1057@gmail.com>
2026-06-26 10:00:00 +0530

    extract shared navItems to lib/nav

---
+++ client/src/lib/nav.tsx
@@ -0,0 +1,8 @@
+import { MessageSquare } from "lucide-react";
+
+export const navItems = [
+  { name: "Start Run", link: "/chat", icon: <MessageSquare size={16} /> },
+  { name: "About", link: "/about" },
+  { name: "How it works", link: "/how-it-works" },
+  { name: "Support", link: "/support" },
+];
